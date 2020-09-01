c56f71b5895aaf2db62310b2d9d78dccfc15fdd2
ericdiniz <64018893+ericdiniz@users.noreply.github.com>
2020-09-01 18:03:57 -0300

    Add files via upload

---
+++ AlgebraBooleana.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+// ERIC RODRIGUES DINIZ - 707760
+// TP01Q06 - Is em Java
+
+public class AlgebraBooleana
+{
+   public static void main (String[]args)
+   {
+      
+   }
+}
